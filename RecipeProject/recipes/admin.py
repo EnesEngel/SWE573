4c90e1829df72a34c2fe23808c1659f8185cd98d
EnesEngel <40452002+EnesEngel@users.noreply.github.com>
2023-12-05 15:23:59 +0300

    Delete RecipeProject directory
    Deleted due to the major changes in local project

--- RecipeProject/recipes/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Recipe, Cuisine, Message
-# Register your models here.
-
-admin.site.register(Recipe)
-admin.site.register(Cuisine)
-admin.site.register(Message)
